74313457bc3e3aa2b57d06681360b526a9784c75
Cristiano Nunes <postcristiano@gmail.com>
2023-04-16 14:24:13 -0300

    2023-04-16 14:24:13

---
+++ auto-git-pull-push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+MY_PATH="<path to your code>"
+
+cd "$MY_PATH"
+
+git pull 
+
+CHANGES_EXIST="$(git status — porcelain | wc -l)"
+
+if [ "$CHANGES_EXIST" -eq 0 ]; then 
+exit 0
+fi 
+
+git add .; git commit -q -m "$(date +"%Y-%m-%d %H:%M:%S")"; git push -q
