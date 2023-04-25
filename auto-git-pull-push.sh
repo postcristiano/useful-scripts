101c27670f8bf2c0f2699648fa73e691feb47a14
Cristiano Nunes <postcristiano@gmail.com>
2023-04-25 20:44:09 -0300

    2023-04-25 20:44:09

--- auto-git-pull-push.sh
+++ auto-git-pull-push.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-
+# Set path to your code
 MY_PATH="<path to your code>"
 
+# Download the code from the repository if it is more up-to-date
 cd "$MY_PATH"
 git pull 
 
@@ -12,4 +13,5 @@ if [ "$CHANGES_EXIST" -eq 0 ]; then
     exit 0
 fi 
 
+# Upload the local code
 git add .; git commit -q -m "$(date +"%Y-%m-%d %H:%M:%S")"; git push -q
